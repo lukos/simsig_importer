743a9767d92085adf2bb5f36a9d46b421bc7e2ba
Luke Briner <lukiebriner@gmail.com>
2022-02-20 17:39:18 +0000

    Day selector tests

---
+++ SimsigImporterLibrary/Helpers/Validators.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+namespace SimsigImporterLib.Helpers
+{
+    /// <summary>
+    /// Validation helpers
+    /// </summary>
+    public static class Validators
+    {
+        private static Regex daysCode = new Regex("^((M|T|W|Th|F|S){1,6}(O|X))|SUN$", RegexOptions.Compiled);
+
+        /// <summary>
+        /// Verify the code for the day restriction on a service. Only needs to validate codes, not blanks.
+        /// </summary>
+        /// <param name="input">The days input</param>
+        /// <returns>True if the code represents a valid set of days, otherwise false</returns>
+        public static bool VerifyDayCode(string input)
+        {
+            return input.IsPresent() && daysCode.IsMatch(input);
+        }
+    }
+}
